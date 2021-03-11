423fec009af49a69c9db49f6d39ffce28c844a41
bloges56 <brady.drew.logan@gmail.com>
2021-03-11 10:37:49 -0600

    enemy now follows the player

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
+    GameObject player;
+    [SerializeField] float enemySpeed = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate((player.transform.position - transform.position).normalized * enemySpeed);
     }
 }
